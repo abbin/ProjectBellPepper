87bdf36ae536d40ca75a64561f014238ae3bb02d
abbin <abn@fuudapp.com>
2016-10-14 15:34:27 +0530

    Initial Commit

---
+++ ProjectBellPepper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectBellPepper
+//
+//  Created by Abbin Varghese on 14/10/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
